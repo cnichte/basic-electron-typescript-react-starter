e2d0c0e06340c9db14ce1b05137809f10d035236
Carsten Nichte <c.nichte@t-online.de>
2024-09-29 18:46:25 +0200

    fix

--- src/app/backend/Database_Pouchdb_Initialize.ts
+++ src/app/backend/Database_Pouchdb_Initialize.ts
@@ -1,8 +1,9 @@
 import { DocBook } from "../common/types/DocBook";
 import { DocUser } from "../common/types/DocUser";
+import { DatabaseCRUD_Interface } from "./Database_Types";
 import { DocumentCreator } from "./DocumentCreator";
 
-export function db_initialize(db:any) {
+export function db_initialize(db:DatabaseCRUD_Interface) {
   DocumentCreator.loadTo<DocUser>(db, "./docs-json/users.json", DocUser);
   DocumentCreator.loadTo<DocBook>(db, "./docs-json/books.json", DocBook);
 }
